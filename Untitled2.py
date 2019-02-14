560d7f11c3ac5048911ca09f0b799dfbb8adbc35
ChangfaFU <47615569+ChangfaFU@users.noreply.github.com>
2019-02-14 01:37:20 +0100

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <b>Coursera_Capstone</b>
+
+# Changfa FU
+
+# 14.02.2019
+
+# <i>This Notebook is an individual work for IBM Applied Data Science Capstone on Coursera</i>
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as Np
+
+
+# In[3]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
